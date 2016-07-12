17979150baeaef6f0c0be17270fff580fcf5fb9a
Conrad H. Appel, IV <me@conradhappeliv.com>
2016-07-12 11:48:57 -0500

    pitch snapping (just chromatic for now)

--- main.cpp
+++ main.cpp
@@ -20,6 +20,8 @@ using namespace std;
 
 int main() {
     // program parameters
+    int lowest_octave = 1;
+    int highest_octave = 8;
     double smoothing = .65; // portamento/rate of change, 0-1 (0 is instant, 1 never moves)
 
     // globals
@@ -36,14 +38,28 @@ int main() {
     bool mod_on = false;
     bool pitch_lock = false;
     bool wave_lock = false;
+    bool pitch_snap = false;
+    vector<double> chrom_pitches;
     double ampx, ampy;
+
+    // calculate chromatic pitches for snapping
+    {
+        vector<double> pitch_ratios = {1, 16./15, 1.125, 1.2, 1.25, 4./3, 45. / 32, 1.5, 1.6, 5./3, 1.8, 1.875};
+        for (int i = lowest_octave - 1; i < highest_octave + 1; i++) {
+            double A = 440 * pow(2, -4 + i);
+            for(double ratio: pitch_ratios) {
+                chrom_pitches.push_back(A*ratio);
+            }
+        }
+    }
 
     // assign buttons
     // A B X Y LB RB SEL STA Home LS RS
     js.set_button_press_callback(0, [&mod_on]() { mod_on = true; });
     js.set_button_release_callback(0, [&mod_on]() { mod_on = false; });
-    js.set_button_press_callback(2, [&octave](){ if(octave >= 2) octave--; });
-    js.set_button_press_callback(3, [&octave](){ if(octave <= 7) octave++; });
+    js.set_button_press_callback(1, [&pitch_snap]() { pitch_snap = !pitch_snap; });
+    js.set_button_press_callback(2, [&](){ if(octave > lowest_octave) octave--; });
+    js.set_button_press_callback(3, [&](){ if(octave < highest_octave) octave++; });
     js.set_button_press_callback(9, [&pitch_lock]() { pitch_lock = !pitch_lock; });
     js.set_button_press_callback(10, [&wave_lock]() { wave_lock = !wave_lock; });
 
@@ -51,11 +67,21 @@ int main() {
     a.set_callback([&](int n) {
         // course pitch
         double newfreq = prev_freq;
-        if(!pitch_lock) newfreq = 440*pow(2, -4+octave)*(pow(2, js.axis(1)*-1/32767.));
+        if(!pitch_lock) {
+            newfreq = 440*pow(2, -4+octave)*(pow(2, js.axis(1)*-1/32767.));
+            // snapping
+            if(pitch_snap) {
+                auto lower = lower_bound(chrom_pitches.begin(), chrom_pitches.end(), newfreq);
+                if(lower != chrom_pitches.end() && lower != chrom_pitches.end()+1) {
+                    if(newfreq - *lower > *(lower+1) - newfreq) lower++;
+                    newfreq = *lower;
+                }
+            }
+        }
         // fine pitch
         newfreq *= pow(2, js.axis(0)/32767.)/6.-(.5/6+.125-1);
         // portamento
-        if(smoothing) newfreq = (newfreq*(1-smoothing) + prev_freq*(1+smoothing)) / 2.;
+        if(smoothing && !pitch_snap) newfreq = (newfreq*(1-smoothing) + prev_freq*(1+smoothing)) / 2.;
         // send frequency to the synths
         if(!pitch_lock) prev_freq = newfreq;
         s1.setFrequency(newfreq);
