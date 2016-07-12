924f10f9f2013ac23c504d7fee1eb190a4a7b7d6
Conrad H. Appel, IV <me@conradhappeliv.com>
2016-07-12 11:06:58 -0500

    pitch/wave lock on LS/RS

--- main.cpp
+++ main.cpp
@@ -34,39 +34,50 @@ int main() {
     int octave = 4;
     double prev_freq = 440;
     bool mod_on = false;
+    bool pitch_lock = false;
+    bool wave_lock = false;
+    double ampx, ampy;
 
     // assign buttons
-    // A B X Y LB RB SEL STA
+    // A B X Y LB RB SEL STA Home LS RS
     js.set_button_press_callback(0, [&mod_on]() { mod_on = true; });
     js.set_button_release_callback(0, [&mod_on]() { mod_on = false; });
     js.set_button_press_callback(2, [&octave](){ if(octave >= 2) octave--; });
     js.set_button_press_callback(3, [&octave](){ if(octave <= 7) octave++; });
+    js.set_button_press_callback(9, [&pitch_lock]() { pitch_lock = !pitch_lock; });
+    js.set_button_press_callback(10, [&wave_lock]() { wave_lock = !wave_lock; });
 
     // main audio callback
     a.set_callback([&](int n) {
-        // course * fine pitch
-        double newfreq = 440*pow(2, -4+octave)*(pow(2, js.axis(1)*-1/32767.));
+        // course pitch
+        double newfreq = prev_freq;
+        if(!pitch_lock) newfreq = 440*pow(2, -4+octave)*(pow(2, js.axis(1)*-1/32767.));
+        // fine pitch
         newfreq *= pow(2, js.axis(0)/32767.)/6.-(.5/6+.125-1);
+        // portamento
         if(smoothing) newfreq = (newfreq*(1-smoothing) + prev_freq*(1+smoothing)) / 2.;
-        prev_freq = newfreq;
+        // send frequency to the synths
+        if(!pitch_lock) prev_freq = newfreq;
+        s1.setFrequency(newfreq);
+        s2.setFrequency(newfreq);
+        s3.setFrequency(newfreq);
+        s4.setFrequency(newfreq);
 
         // amplitude
-        double overallamp = (js.axis(2) + 32768)/65536.;
-        double ampx = js.axis(3)/32767.;
-        double ampy = js.axis(4)*-1/32767.;
+        double overallamp = (js.axis(2) + 32768) / 65536.;
+        if(!wave_lock) {
+            ampx = js.axis(3) / 32767.;
+            ampy = js.axis(4) * -1 / 32767.;
+        }
+        s1.setAmplitude(overallamp * (.5 + .5 * ampy));
+        s2.setAmplitude(overallamp * (.5 + .5 * ampy * -1));
+        s3.setAmplitude(overallamp * (.5 + .5 * ampx));
+        s4.setAmplitude(overallamp * (.5 + .5 * ampx * -1));
 
         s1.setMod(mod_on);
         s2.setMod(mod_on);
         s3.setMod(mod_on);
         s4.setMod(mod_on);
-        s1.setFrequency(newfreq);
-        s2.setFrequency(newfreq);
-        s3.setFrequency(newfreq);
-        s4.setFrequency(newfreq);
-        s1.setAmplitude(overallamp * (.5 + .5*ampy));
-        s2.setAmplitude(overallamp * (.5 + .5*ampy*-1));
-        s3.setAmplitude(overallamp * (.5 + .5*ampx));
-        s4.setAmplitude(overallamp * (.5 + .5*ampx*-1));
 
         vector<double> res1, res2, res3, res4;
         s1.getBuffer(res1, n);
